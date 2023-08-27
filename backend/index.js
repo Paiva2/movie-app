9a13cc934a32eb34e7392eba9e41277ccc204f3f
unknown <joaovitor.paiva145@hotmail.com>
2023-08-27 01:24:05 -0300

    feat/register new user on backend (no jwt yet)

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
-const path = require("path")
-require("dotenv").config({ path: path.resolve(__dirname, "./process.env") })
+import express from "express"
+import axios from "axios"
+import bcrypt from "bcrypt"
+import cors from "cors"
+import "dotenv/config"
+import prisma from "./lib/prisma.js"
 
-const { default: axios } = require("axios")
-const express = require("express")
 const app = express()
+
 const port = 3000
-const cors = require("cors")
+
 app.use(cors())
 app.use(express.json())
+
+function encryptPassword(saltRounds = 10, data) {
+  const passwordToHash = data
+  const hashedPassword = bcrypt.hashSync(passwordToHash, saltRounds)
+
+  return hashedPassword
+}
 
 const TmdbOptions = {
   method: "GET",
@@ -33,6 +43,45 @@ app.get("/trending-movies", async (_, res) => {
   )
 
   return res.status(200).send(response.data.results)
+})
+
+app.post("/register", async (req, res) => {
+  if (!req.body.data.username) {
+    if (typeof req.body.data.username !== "string") {
+      return res.status(404).send("Username must be a string.")
+    }
+
+    return res.status(404).send("Username or Password can't be empty.")
+  } else if (!req.body.data.password) {
+    if (typeof req.body.data.password !== "string") {
+      return res.status(404).send("Password must be a string.")
+    }
+
+    return res.status(404).send("Password or Password can't be empty.")
+  }
+
+  const isUserRegistered = await prisma.user.findUnique({
+    where: {
+      username: req.body.data.username,
+    },
+  })
+
+  if (isUserRegistered) {
+    return res.status(409).send("Username already exists.")
+  }
+
+  const hashedPassword = encryptPassword(10, req.body.data.password)
+
+  await prisma.user.create({
+    data: {
+      username: req.body.data.username,
+      password: hashedPassword,
+      image:
+        req.body.data.image ?? "https://i.postimg.cc/XvrwMSKy/transferir.jpg",
+    },
+  })
+
+  return res.status(201).send("User created with success!")
 })
 
 app.listen(port, () => {
